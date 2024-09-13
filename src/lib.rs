fcbd96e405349ee85ba155c13c26349492515be7
ivajon <ivar.jonsson18@gmail.com>
2024-09-13 17:58:01 +0200

    Pesky linker

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,9 @@ pub mod wrapper;
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is
 // invoked
-#[defmt::panic_handler]
-fn panic() -> ! {
+#[allow(dead_code)]
+#[inline(never)]
+extern "C" fn _defmt_panic() -> ! {
     cortex_m::asm::udf()
 }
 
