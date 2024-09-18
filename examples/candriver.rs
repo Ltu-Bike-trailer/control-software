1dcfe4ca5d989f1c95da0ac3953f6479d3f73a7c
wiiggee1 <viktor@smedberg.eu>
2024-09-18 18:58:07 +0200

    adding --example with rtic, and writing can module logic continued

--- examples/candriver.rs
+++ examples/candriver.rs
@@ -2,40 +2,75 @@
 #![no_main]
 #![allow(unused)]
 
-use controller::drivers::can::CanDriver;
+use controller::drivers::can::{CanDriver, CanModule, CanSettings, CanMessage};
 use nrf52840_hal as _;
 use nrf52840_hal::gpio::{Level, Port};
 use cortex_m::asm as _;
 use defmt_rtt as _;
 use panic_probe as _;
 use cortex_m_rt::entry;
+use rtic::app;
 
 use controller as _;
 
 
 
-#[entry]
-fn main() -> ! {
-    defmt::info!("init");
-    defmt::println!("...");
-
-    let periperal = nrf52840_hal::pac::Peripherals::take().unwrap();
-    let port0 = nrf52840_hal::gpio::p0::Parts::new(periperal.P0);
+#[rtic::app(device = nrf52840_hal::pac, dispatchers = [RTC0])]
+mod app {
+    use controller::drivers::can::{CanDriver, CanModule, CanSettings, CanMessage, OperationTypes};
+    use cortex_m::asm;
+    use embedded_hal::digital::OutputPin;
+    use nrf52840_hal::{pac::SPI0, spi::Spi, spi::Frequency, spim::*};
+    use nrf52840_hal::gpio::{self, Level, Port, Pin, Output, PushPull};
     
-    defmt::println!("Initialize the SPI instance, and CanDriver");
+    
+    #[shared]
+    struct Shared{}
 
-    let pins = nrf52840_hal::spi::Pins{
-        sck: Some(port0.p0_21.into_push_pull_output(Level::Low).degrade()),
-        mosi: Some(port0.p0_22.into_push_pull_output(Level::Low).degrade()),
-        miso: Some(port0.p0_23.into_floating_input().degrade()),
-    };
+    #[local]
+    struct Local {
+        candriver: CanDriver<Spi<SPI0>, Pin<Output<PushPull>>>,
+    }
 
-    let mut spi = nrf52840_hal::spi::Spi::new(periperal.SPI0, pins, nrf52840_hal::spi::Frequency::K500, nrf52840_hal::spim::MODE_0);
+    #[init]
+    fn init(mut cx: init::Context) -> (Shared, Local){
+        defmt::info!("init");
+        let device = cx.device;
+        let port0 = gpio::p0::Parts::new(device.P0);
+         
+        defmt::println!("Initialize the SPI instance, and CanDriver");
+        let pins = nrf52840_hal::spi::Pins{
+            sck: Some(port0.p0_21.into_push_pull_output(Level::Low).degrade()),
+            mosi: Some(port0.p0_22.into_push_pull_output(Level::Low).degrade()),
+            miso: Some(port0.p0_23.into_floating_input().degrade()),
+        };
 
-    let candriver = CanDriver::init(spi);
-     
-
-    loop {
+        let mut spi = Spi::new(device.SPI0, pins, Frequency::K500, MODE_0);
+        let cs_pin = port0.p0_24.into_push_pull_output(Level::High).degrade(); 
         
+        let can_settings = CanSettings{
+            mode: OperationTypes::Loopback,
+            can_clk: 0,
+            can_bitrate: 0, 
+
+        };
+
+        let mut can_driver = CanDriver::init(spi, cs_pin, can_settings);
+        
+        defmt::println!("After initializing Spi<SPI0>..."); 
+
+        (
+            Shared {},
+            Local {candriver: can_driver},
+        )
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::info!("idle");
+
+        loop {
+            asm::wfi(); 
+        }
     }
 }
