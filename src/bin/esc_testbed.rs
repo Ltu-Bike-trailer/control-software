d3760b718cda968ee0b8fa9cee8352d89d298cc6
ivajon <ivar.jonsson18@gmail.com>
2024-10-06 17:21:39 +0200

    Refactor to make it more reliable hopefully

--- src/bin/esc_testbed.rs
+++ src/bin/esc_testbed.rs
@@ -20,7 +20,10 @@ nrf_rtc0_monotonic!(Mono);
 )]
 mod app {
 
-    use controller::bldc::{DrivePattern, FloatDuty};
+    use controller::{
+        bldc::{DrivePattern, FloatDuty},
+        cart::constants::{CURRENT_SAMPLE_RATE, PHASE_SHIFT_HAL, WHEEL_DIAMETER},
+    };
     use embedded_hal::digital::InputPin;
     use esc::{self, events::GpioEvents, PinConfig};
     use lib::protocol;
@@ -30,7 +33,7 @@ mod app {
         pwm::{self, Pwm},
         time::U32Ext,
     };
-    use rtic_monotonics::{fugit::ExtU64, Monotonic};
+    use rtic_monotonics::Monotonic;
 
     use crate::Mono;
 
@@ -41,6 +44,10 @@ mod app {
         drive_pattern: DrivePattern,
         sender: protocol::sender::Sender<10>,
         duty: f32,
+        phase1: Pwm<PWM0>,
+        phase2: Pwm<PWM1>,
+        phase3: Pwm<PWM2>,
+        velocity: f32,
     }
 
     // Local resources go here
@@ -50,14 +57,14 @@ mod app {
         events: esc::events::Manager,
         current_sense: esc::CurrentManager,
         hal_pins: [Pin<Input<PullUp>>; 3],
-        phase1: Pwm<PWM0>,
-        phase2: Pwm<PWM1>,
-        phase3: Pwm<PWM2>,
     }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
         defmt::info!("init");
+        nrf52840_hal::Clocks::new(cx.device.CLOCK)
+            .enable_ext_hfosc()
+            .start_lfclk();
         Mono::start(cx.device.RTC0);
 
         let p0 = nrf52840_hal::gpio::p0::Parts::new(cx.device.P0);
@@ -96,9 +103,9 @@ mod app {
         let drive_pattern = DrivePattern::new();
 
         let sender = protocol::sender::Sender::new();
+        debug_assert!(Mono::now().duration_since_epoch().to_micros() != 0);
 
         // Spawn the tasks.
-        sense::spawn().ok().unwrap();
         motor_driver::spawn().ok().unwrap();
         (
             Shared {
@@ -106,102 +113,92 @@ mod app {
                 drive_pattern,
                 sender,
                 duty: 0.2,
+                phase1,
+                phase2,
+                phase3,
+                velocity: 0.,
             },
             Local {
                 // Initialization of local resources go here
                 events,
                 current_sense,
                 hal_pins,
-                phase1,
-                phase2,
-                phase3,
             },
         )
     }
 
-    #[task(binds=GPIOTE, local=[events,hal_pins],shared = [drive_pattern], priority = 5)]
+    #[task(binds=GPIOTE, local=[events,hal_pins,t:Option<u64> = None],shared = [drive_pattern,phase1,phase2,phase3,duty,velocity], priority = 5)]
     /// Manages gpiote interrupts.
     ///
     /// This is every pin interrupt. So hal effect feedback, can bus messages
     /// and possibly more. the contents of this function will vary with the
     /// board we are using.
+    ///
+    /// ## Motor driving
+    ///
+    /// This function also drives the motors. This is the defualt case. The only
+    /// other time that the motor should be driven externally is if the PID
+    /// controller requests a new velocity.
+    ///
+    /// This ensures that the motor can be started from standstill.
     fn handle_gpio(mut cx: handle_gpio::Context) {
+        let t = Mono::now();
         // Docs says that it is infallible.
         let edge =
             |pin: &mut Pin<Input<PullUp>>| -> bool { unsafe { pin.is_high().unwrap_unchecked() } };
         let mut set_phase = false;
+        let mut positive_flank = false;
         for event in cx.local.events.events() {
             match event {
                 GpioEvents::P1Hal => {
-                    cx.shared
-                        .drive_pattern
-                        .lock(|pattern| match edge(&mut cx.local.hal_pins[0]) {
-                            true => pattern.set_a(),
-                            false => pattern.clear_a(),
-                        });
-                    set_phase |= true;
+                    defmt::trace!("P1 hal");
+                    let edge = edge(&mut cx.local.hal_pins[0]);
+                    cx.shared.drive_pattern.lock(|pattern| match edge {
+                        true => pattern.set_a(),
+                        false => pattern.clear_a(),
+                    });
+                    set_phase = true;
+                    positive_flank |= edge;
                 }
                 GpioEvents::P2Hal => {
-                    cx.shared
-                        .drive_pattern
-                        .lock(|pattern| match edge(&mut cx.local.hal_pins[0]) {
-                            true => pattern.set_b(),
-                            false => pattern.clear_b(),
-                        });
-                    set_phase |= true;
+                    defmt::trace!("P2 hal");
+                    let edge = edge(&mut cx.local.hal_pins[1]);
+                    cx.shared.drive_pattern.lock(|pattern| match edge {
+                        true => pattern.set_b(),
+                        false => pattern.clear_b(),
+                    });
+                    set_phase = true;
+                    positive_flank |= edge;
                 }
                 GpioEvents::P3Hal => {
-                    cx.shared
-                        .drive_pattern
-                        .lock(|pattern| match edge(&mut cx.local.hal_pins[0]) {
-                            true => pattern.set_c(),
-                            false => pattern.clear_c(),
-                        });
-                    set_phase |= true;
+                    defmt::trace!("P3 hal");
+                    let edge = edge(&mut cx.local.hal_pins[2]);
+                    cx.shared.drive_pattern.lock(|pattern| match edge {
+                        true => pattern.set_c(),
+                        false => pattern.clear_c(),
+                    });
+                    set_phase = true;
+                    positive_flank |= edge;
                 }
                 GpioEvents::CAN => todo!("Handle CAN event"),
             }
         }
 
-        if set_phase {
-            defmt::info!("Got hal effect interrupt!");
+        if !set_phase {
+            return;
         }
-    }
 
-    #[task(local=[current_sense],shared = [sender],priority = 2)]
-    /// Measures the current consumption at the current timestamp.
-    ///
-    /// TODO: Rewrite the hal implementation of this to not be blocking so we
-    /// can sample faster.
-    async fn sense(mut cx: sense::Context) {
-        loop {
-            let t = Mono::now();
-            let measurement = match cx.local.current_sense.sample() {
-                Ok(val) => val,
-                Err(_) => continue,
-            };
-            let _ = cx.shared.sender.lock(|sender| {
-                sender.set_current_sense_left((t.duration_since_epoch().to_millis(), measurement))
-            });
-            // Poll each 10 ms.
-            Mono::delay_until(t + 10u64.millis()).await;
-        }
-    }
-
-    #[task(shared = [drive_pattern,duty],local = [phase1,phase2,phase3],priority = 2)]
-    /// Drives the phases of the motor.
-    ///
-    /// This is done by disabling the high/low side of each phase with respect
-    /// to the switching pattern.
-    async fn motor_driver(cx: motor_driver::Context) {
-        let (phase1, phase2, phase3) = (cx.local.phase1, cx.local.phase2, cx.local.phase3);
+        // Trigger the phases inline since we want to run it as fast as possible.
+        //
+        // Doing it this way ensures that we never miss a state, given that the app i
+        // schedulable.
+        defmt::info!("Got hal effect interrupt!");
         let (mut drive_pattern, mut duty) = (cx.shared.drive_pattern, cx.shared.duty);
-        loop {
-            let t = Mono::now();
 
-            let ((p1, p2, p3), duty) =
-                (&mut drive_pattern, &mut duty).lock(|pattern, duty| (pattern.get(), *duty));
-            defmt::trace!("Pattern A {}, B {}, C {}", p1, p2, p3);
+        let ((p1, p2, p3), duty) =
+            (&mut drive_pattern, &mut duty).lock(|pattern, duty| (pattern.get(), *duty));
+        defmt::trace!("Pattern A {}, B {}, C {}", p1, p2, p3);
+        (cx.shared.phase1, cx.shared.phase2, cx.shared.phase3).lock(|phase1, phase2, phase3| {
             match p1 {
                 true => phase1
                     .enable_channel(pwm::Channel::C0)
@@ -229,19 +226,107 @@ mod app {
             phase1.set_duty(duty);
             phase2.set_duty(duty);
             phase3.set_duty(duty);
+        });
 
-            // Poll each 10 us.
-            Mono::delay_until(t + 10u64.micros()).await;
+        // Estimate velocity based on latest two measurements.
+        if !positive_flank {
+            return;
         }
+
+        let t = t.duration_since_epoch().to_micros();
+        let t_old = cx.local.t.replace(t);
+        if t_old.is_none() {
+            return;
+        }
+        let dt = t - unsafe { t_old.unwrap_unchecked() };
+        let dt = (dt as f32) / 1_000_000.;
+        let radial_vel = PHASE_SHIFT_HAL / dt;
+        let linear_vel = WHEEL_DIAMETER * radial_vel;
+        defmt::info!("Velocity : {} [m/s]", linear_vel);
+        cx.shared.velocity.lock(|vel| *vel = linear_vel);
     }
 
-    // Optional idle, can be removed if not needed.
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
+    #[task(shared = [drive_pattern,duty,phase1,phase2,phase3],priority = 2)]
+    /// Drives the phases of the motor.
+    ///
+    /// This is done by disabling the high/low side of each phase with respect
+    /// to the switching pattern.
+    ///
+    /// This function should only be called by the PID controller.
+    ///
+    /// This ensures that the velocity can be set without dependance on the rate
+    /// of interrupts.
+    async fn motor_driver(cx: motor_driver::Context) {
+        let (mut drive_pattern, mut duty) = (cx.shared.drive_pattern, cx.shared.duty);
+
+        let ((p1, p2, p3), duty) =
+            (&mut drive_pattern, &mut duty).lock(|pattern, duty| (pattern.get(), *duty));
+        defmt::info!("Pattern A {}, B {}, C {}", p1, p2, p3);
+        (cx.shared.phase1, cx.shared.phase2, cx.shared.phase3).lock(|phase1, phase2, phase3| {
+            match p1 {
+                true => phase1
+                    .enable_channel(pwm::Channel::C0)
+                    .disable_channel(pwm::Channel::C1),
+                false => phase1
+                    .enable_channel(pwm::Channel::C1)
+                    .disable_channel(pwm::Channel::C0),
+            };
+            match p2 {
+                true => phase2
+                    .enable_channel(pwm::Channel::C0)
+                    .disable_channel(pwm::Channel::C1),
+                false => phase2
+                    .enable_channel(pwm::Channel::C1)
+                    .disable_channel(pwm::Channel::C0),
+            };
+            match p3 {
+                true => phase3
+                    .enable_channel(pwm::Channel::C0)
+                    .disable_channel(pwm::Channel::C1),
+                false => phase3
+                    .enable_channel(pwm::Channel::C1)
+                    .disable_channel(pwm::Channel::C0),
+            };
+            phase1.set_duty(duty);
+            phase2.set_duty(duty);
+            phase3.set_duty(duty);
+        });
+    }
+
+    /// When ever the system is not running we should sample the current.
+    #[idle(local=[current_sense],shared = [sender])]
+    fn idle(mut cx: idle::Context) -> ! {
         defmt::info!("idle");
 
+        let mut count = 0;
+        let (mut avg_p1, mut avg_p2, mut avg_p3) = (0., 0., 0.);
+        let mut prev = Mono::now();
         loop {
-            continue;
+            let t = Mono::now();
+            let [p1, p2, p3] = match cx.local.current_sense.sample() {
+                Ok(val) => val,
+                Err(_) => continue,
+            };
+
+            avg_p1 += p1;
+            avg_p2 += p2;
+            avg_p3 += p3;
+            count += 1;
+
+            // Send the average over the latest measurement period.
+            if t.checked_duration_since(prev)
+                .is_some_and(|diff| diff.to_micros() > CURRENT_SAMPLE_RATE)
+            {
+                defmt::info!("Sending current average");
+                prev = t;
+                let _ = cx.shared.sender.lock(|sender| {
+                    sender.set_current_sense_left((t.duration_since_epoch().to_millis(), [
+                        avg_p1 / count as f32,
+                        avg_p2 / count as f32,
+                        avg_p3 / count as f32,
+                    ]))
+                });
+            }
         }
     }
 }
