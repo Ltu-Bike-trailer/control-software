20bec3f0fa8d7f2bee649b1702aa1a824eb387d2
ivajon <ivar.jonsson18@gmail.com>
2024-10-22 13:56:08 +0200

    Zpeed

--- src/cart.rs
+++ src/cart.rs
@@ -15,23 +15,27 @@ pub mod constants {
 
 /// Defines the controllers used in the cart.
 pub mod controllers {
-    use lib::pid::PidDynamic;
+    use lib::pid::Pid;
 
     /// The Proportional gain for the motor.
     pub const MOTOR_KP: i32 = 10;
     /// The Integral gain for the motor.
-    pub const MOTOR_KI: i32 = 10;
+    pub const MOTOR_KI: i32 = 1;
     /// The derivative gain for the motor.
-    pub const MOTOR_KD: i32 = 10;
+    pub const MOTOR_KD: i32 = 1;
+    /// Motor sample time in the [`MOTOR_TIMESCALE`] time-frame.
+    pub const MOTOR_TS: i32 = 10_000;
+    /// Motor time resolution
+    pub const MOTOR_TIMESCALE: i32 = 1_000_000;
     /// The decimal point in the controller.
     ///
     /// So each KP,KI,KD is divided by this.
-    pub const MOTOR_FIXED_POINT: u32 = 10;
+    pub const MOTOR_FIXED_POINT: u32 = 100;
 
     /// The motor PID controller.
     ///
     /// This type makes it easier to change the controller without sideeffects.
-    pub type MotorPid = PidDynamic<
+    pub type MotorPid = Pid<
         (),
         f32,
         f32,
@@ -40,11 +44,12 @@ pub mod controllers {
         MOTOR_KP,
         MOTOR_KI,
         MOTOR_KD,
+        MOTOR_TS,
         // We want percentages.
         1,
         0,
         // We will use uS as timescale.
-        1_000_000,
+        MOTOR_TIMESCALE,
         MOTOR_FIXED_POINT,
     >;
 }
