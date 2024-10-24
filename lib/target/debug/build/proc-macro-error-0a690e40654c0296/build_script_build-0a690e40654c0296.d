3402ba102d7be0c2557b410ee62821af61bb6687
wiiggee1 <viktor@smedberg.eu>
2024-10-24 16:29:39 +0200

    removing lib/target

--- lib/target/debug/build/proc-macro-error-0a690e40654c0296/build_script_build-0a690e40654c0296.d
+++
@@ -1,5 +0,0 @@
-/home/wiiggee1/Desktop/School/D7039E/control-software/lib/target/debug/build/proc-macro-error-0a690e40654c0296/build_script_build-0a690e40654c0296: /home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro-error-1.0.4/build.rs
-
-/home/wiiggee1/Desktop/School/D7039E/control-software/lib/target/debug/build/proc-macro-error-0a690e40654c0296/build_script_build-0a690e40654c0296.d: /home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro-error-1.0.4/build.rs
-
-/home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro-error-1.0.4/build.rs:
--- lib/target/debug/build/proc-macro2-ac60fa7fb2fa6d4f/build_script_build-ac60fa7fb2fa6d4f.d
+++
@@ -1,5 +0,0 @@
-/home/wiiggee1/Desktop/School/D7039E/control-software/lib/target/debug/build/proc-macro2-ac60fa7fb2fa6d4f/build_script_build-ac60fa7fb2fa6d4f: /home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/wiiggee1/Desktop/School/D7039E/control-software/lib/target/debug/build/proc-macro2-ac60fa7fb2fa6d4f/build_script_build-ac60fa7fb2fa6d4f.d: /home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/wiiggee1/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs:
