d6a46daefebb06b42af3c3bd98764d070740fd9f
wiiggee1 <viktor@smedberg.eu>
2024-10-29 11:40:35 +0100

    cargo fmt check fixes

--- examples/candriver_protocol.rs
+++ examples/candriver_protocol.rs
@@ -3,9 +3,7 @@
 #![allow(unused)]
 
 use controller as _;
-use controller::drivers::{
-    can::{Mcp2515Driver, Mcp2515Settings},
-};
+use controller::drivers::can::{Mcp2515Driver, Mcp2515Settings};
 use cortex_m::asm as _;
 use cortex_m_rt::entry;
 use defmt_rtt as _;
@@ -19,24 +17,23 @@ mod app {
 
     use controller::{
         boards::*,
-        drivers::{
-            can::{
-                AcceptanceFilterMask,
-                Bitrate,
-                Mcp2515Driver,
-                Mcp2515Settings,
-                McpClock,
-                OperationTypes,
-                ReceiveBufferMode,
-                SettingsCanCtrl,
-                CLKPRE,
-                RXBN,
-            },
+        drivers::can::{
+            AcceptanceFilterMask,
+            Bitrate,
+            Mcp2515Driver,
+            Mcp2515Settings,
+            McpClock,
+            OperationTypes,
+            ReceiveBufferMode,
+            SettingsCanCtrl,
+            CLKPRE,
+            RXBN,
         },
     };
     use cortex_m::asm;
     use embedded_can::{blocking::Can, Frame, StandardId};
     use embedded_hal::{digital::OutputPin, spi::SpiBus};
+    use lib::protocol::sender::Sender;
     use nrf52840_hal::{
         gpio::{self, Floating, Input, Level, Output, Pin, Port, PullUp, PushPull},
         gpiote::{Gpiote, GpioteInputPin},
@@ -46,7 +43,6 @@ mod app {
         spim::*,
         Clocks,
     };
-    use lib::protocol::sender::Sender;
     use rtic_monotonics::nrf::{
         self,
         rtc::*,
@@ -134,7 +130,7 @@ mod app {
             ABAT,
             OSM,
         );
-        
+
         let can_settings = Mcp2515Settings::new(
             canctrl_settings,
             McpClock::MCP8,
@@ -164,7 +160,7 @@ mod app {
         defmt::println!("After initializing Spi<SPI1>...");
         let dummy_id = StandardId::new(0x2).unwrap();
         defmt::info!("dummy_data: {:?}", dummy_data.len());
-        
+
         let mut sender = Sender::new();
         sender.set_left_motor(1.0).unwrap();
         let mut msg = sender.dequeue().unwrap();
@@ -172,7 +168,8 @@ mod app {
         can_driver.transmit(&msg);
 
         //let mut frame =
-         //   CanMessage::new(embedded_can::Id::Standard(dummy_id), &[0x01, 0x02, 0x03]).unwrap();
+        //   CanMessage::new(embedded_can::Id::Standard(dummy_id), &[0x01, 0x02,
+        // 0x03]).unwrap();
 
         //can_driver.loopback_test(frame);
         //can_driver.transmit(&frame);
@@ -202,7 +199,6 @@ mod app {
                 defmt::info!("GPIOTE interrupt occurred [channel 0] - Can Master!");
                 let interrupt_type = cx.local.candriver.interrupt_decode().unwrap();
                 cx.local.candriver.handle_interrupt(interrupt_type);
-
 
                 if (cx.local.candriver.interrupt_is_cleared()) {
                     defmt::info!("All CAN interrupt has been handled!");
