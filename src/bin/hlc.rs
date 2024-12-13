87cfc763c4a64d20b90f891974fc8ee8e2fb27dc
wiiggee1 <viktor@smedberg.eu>
2024-12-13 17:11:36 +0100

    rebasing with main

--- src/bin/hlc.rs
+++ src/bin/hlc.rs
@@ -1,23 +1,35 @@
 #![no_std]
 #![no_main]
 #![allow(unused)]
+<<<<<<< HEAD
 #![feature(generic_arg_infer)]
 #![allow(clippy::field_reassign_with_default)]
 
 use controller as _;
 use defmt::*;
 use defmt_rtt as _;
+=======
+
+use controller as _;
+use defmt_rtt as _;
+use defmt::*;
+>>>>>>> 86eed1f (rebasing with main)
 use nrf52840_hal as _;
 use panic_probe as _;
 use rtic_monotonics::nrf::rtc::prelude::*;
 nrf_rtc0_monotonic!(Mono);
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 86eed1f (rebasing with main)
 #[rtic::app(device = nrf52840_hal::pac, dispatchers = [RTC1, TIMER0, TIMER1])]
 mod hlc {
 
     use can_mcp2515::drivers::{can::*, message::CanMessage};
     use controller::{
         boards::*,
+<<<<<<< HEAD
         drivers::hx711::{Gain, Hx711Driver, ValidTimings},
         hlc_utils::{config::*, core::*, events::*, stype_calibration::*},
     };
@@ -58,18 +70,42 @@ mod hlc {
         make_channel,
     };
 
+=======
+    };
+    use cortex_m::asm;
+    use defmt::Debug2Format;
+    use embedded_can::{blocking::Can, StandardId};
+    use embedded_hal::*;
+    use lib::protocol::{sender::Sender, constants::*};
+    use nrf52840_hal::{
+        gpio::{self, Input, Level, Output, Pin, PullUp, PushPull},
+        gpiote::Gpiote,
+        pac::SPI0,
+        spi::{Frequency, Spi},
+        spim::*,
+        Clocks,
+    };
+   
+    use rtic_monotonics::{fugit::ExtU32, Monotonic};
+>>>>>>> 86eed1f (rebasing with main)
     use crate::Mono;
 
     #[shared]
     struct Shared {
+<<<<<<< HEAD
         cargo_weight: f32,
         s_type_force: f32,
         gpiote: Gpiote,
+=======
+        gpiote: Gpiote,
+        candriver: Mcp2515Driver<Spi<SPI0>, Pin<Output<PushPull>>, Pin<Input<PullUp>>>,
+>>>>>>> 86eed1f (rebasing with main)
         sender: Sender<10>,
     }
 
     #[local]
     struct Local {
+<<<<<<< HEAD
         stype: SaadcTask<1>,
         hx711: Hx711Driver<Pin<Output<PushPull>>, Pin<Input<Floating>>>,
         candriver: Mcp2515Driver<Spi<SPI0>, Pin<Output<PushPull>>, Pin<Input<PullUp>>>,
@@ -77,6 +113,9 @@ mod hlc {
         control_reference_sender: channel::Sender<'static, Option<CanMessage>, 10>,
         can_receiver: channel::Receiver<'static, Option<CanMessage>, 10>,
         hlc_controller: Controller,
+=======
+        //candriver: Mcp2515Driver<Spi<SPI0>, Pin<Output<PushPull>>, Pin<Input<PullUp>>>,
+>>>>>>> 86eed1f (rebasing with main)
     }
 
     #[init]
@@ -85,9 +124,25 @@ mod hlc {
         let device = cx.device;
         let port0 = gpio::p0::Parts::new(device.P0);
         let port1 = gpio::p1::Parts::new(device.P1);
+<<<<<<< HEAD
 
         let clk = Clocks::new(device.CLOCK).enable_ext_hfosc().start_lfclk();
         Mono::start(device.RTC0);
+=======
+        let t = nrf52840_hal::pac::Peripherals::take();
+        let clk = Clocks::new(device.CLOCK).enable_ext_hfosc().start_lfclk();
+        Mono::start(device.RTC0);
+        /* Disable shared peripheral addresses */
+
+        device.SPIM0.enable.write(|w| w.enable().disabled());
+        device.SPIS0.enable.write(|w| w.enable().disabled());
+        device.TWIM0.enable.write(|w| w.enable().disabled());
+        device.TWIS0.enable.write(|w| w.enable().disabled());
+        device.TWI0.enable.write(|w| w.enable().disabled());
+
+        //let pin_mapping = PinMapping::new(port0, port1);
+        //let (pin_map, spi) =pin_mapping.can(device.SPIM1);
+>>>>>>> 86eed1f (rebasing with main)
 
         defmt::println!("Initialize the SPI instance, and CanDriver");
         let pins = nrf52840_hal::spi::Pins {
@@ -96,6 +151,7 @@ mod hlc {
             miso: Some(port0.p0_28.into_floating_input().degrade()),
         };
 
+<<<<<<< HEAD
         //let can_manager = CanManager::new(port0, port1);
         //let (cs_pin, int_pin, spi, gpiote) =
         // can_manager.peripheral_instances(device.SPI0, device.GPIOTE);
@@ -106,6 +162,15 @@ mod hlc {
         let mut spi = Spi::new(device.SPI0, pins, Frequency::M1, MODE_0);
         let mut gpiote = Gpiote::new(device.GPIOTE);
 
+=======
+        let cs_pin = port1.p1_02.into_push_pull_output(Level::High).degrade();
+        let can_interrupt = port1.p1_15.into_pullup_input().degrade();
+
+        let mut spi = Spi::new(device.SPI0, pins, Frequency::K125, MODE_0);
+        let mut gpiote = Gpiote::new(device.GPIOTE);
+
+
+>>>>>>> 86eed1f (rebasing with main)
         const CLKEN: bool = true;
         const OSM: bool = false;
         const ABAT: bool = false;
@@ -123,6 +188,7 @@ mod hlc {
             OSM,
         );
 
+<<<<<<< HEAD
         let mut can_settings = Mcp2515Settings::default().enable_interrupts(&[
             CanInte::MERRE,
             CanInte::ERRIE,
@@ -133,6 +199,9 @@ mod hlc {
             CanInte::RX0IE,
         ]);
 
+=======
+        let can_settings = Mcp2515Settings::default();
+>>>>>>> 86eed1f (rebasing with main)
         let mut can_driver = Mcp2515Driver::init(spi, cs_pin, can_interrupt, can_settings);
 
         gpiote
@@ -141,6 +210,7 @@ mod hlc {
             .hi_to_lo()
             .enable_interrupt();
 
+<<<<<<< HEAD
         let (send, receive) = make_channel!(Option<CanMessage>, 10);
         let control_reference_sender = send.clone();
         let mut sender = Sender::new();
@@ -184,11 +254,22 @@ mod hlc {
         //Start sampeling the stype
         adc.start_sample();
         read_hx711::spawn();
+=======
+        defmt::println!("After initializing Spi<SPI1>...");
+        let dummy_id = StandardId::new(0x2).unwrap();
+
+        let mut sender = Sender::new();
+        //sender.set_left_motor(1.0).unwrap();
+
+        send_updates::spawn().unwrap();
+        fetch_data::spawn().unwrap();
+>>>>>>> 86eed1f (rebasing with main)
 
         //let mut frame =
         //   CanMessage::new(embedded_can::Id::Standard(dummy_id), &[0x01, 0x02,
         // 0x03]).unwrap();
 
+<<<<<<< HEAD
         (
             Shared {
                 gpiote,
@@ -206,6 +287,13 @@ mod hlc {
                 control_reference_sender,
             },
         )
+=======
+        //can_driver.transmit(&frame);
+
+        (Shared { gpiote, sender, candriver: can_driver }, Local {
+            
+        })
+>>>>>>> 86eed1f (rebasing with main)
     }
 
     #[idle]
@@ -217,6 +305,7 @@ mod hlc {
         }
     }
 
+<<<<<<< HEAD
     #[task(binds = GPIOTE, shared = [gpiote, sender], local = [can_sender], priority = 4)]
     fn can_interrupt(mut cx: can_interrupt::Context) {
         cx.shared.gpiote.lock(|gpiote| {
@@ -320,3 +409,83 @@ mod hlc {
         });
     }
 }
+=======
+    #[task(binds = GPIOTE, shared = [gpiote, sender, candriver], priority = 5)]
+    fn can_interrupt(mut cx: can_interrupt::Context) {
+        let handle = cx.shared.gpiote.lock(|gpiote| {
+            if (gpiote.channel0().is_event_triggered()) {
+                defmt::println!("\n");
+                defmt::info!("GPIOTE interrupt occurred [channel 0] - Can Master!");
+                
+                cx.shared.candriver.lock(|can_driver|{
+                    let interrupt_type = can_driver.interrupt_decode().unwrap();
+                    if let Some(frame) = can_driver.handle_interrupt(interrupt_type) {
+                        let mut msg_frame = frame;
+                        let std_id = StandardId::new(msg_frame.id_raw()).unwrap();
+                        let recieved_id = Message::try_from(std_id).unwrap();
+
+                        defmt::info!("Received the frame message type: {:?}", Debug2Format(&recieved_id));
+                        //handle_can::spawn(frame).unwrap();
+                        //fetch_data::spawn().unwrap();
+                        //can_driver.transmit(&msg_frame);
+                    }
+                    if can_driver.interrupt_is_cleared() {
+                        defmt::info!("All CAN interrupt has been handled!");
+                        gpiote.channel0().reset_events();
+                    }
+                });
+
+                defmt::println!("\n");
+            }
+            if (gpiote.channel1().is_event_triggered()) {
+                defmt::println!("\n");
+                defmt::info!("GPIOTE interrupt occurred [channel 1] - Can Node!");
+                defmt::println!("\n");
+            }
+
+            //gpiote.reset_events();
+        });
+    }
+
+    #[task(priority = 1, shared = [candriver, sender])]
+    async fn handle_can(mut cx: handle_can::Context, frame: CanMessage){
+        let mut msg_frame = frame;
+        let std_id = StandardId::new(msg_frame.id_raw()).unwrap();
+        let recieved_id = Message::try_from(std_id).unwrap();
+
+        defmt::info!("Received the frame message type: {:?}", Debug2Format(&recieved_id));
+    }
+
+    #[task(priority = 4, shared = [sender])]
+    async fn fetch_data(mut cx: fetch_data::Context){
+        // This is just for testing and keep sending dummy data.
+        loop {
+            cx.shared.sender.lock(|sender|{
+                sender.set_theta(1.2_f32);
+                sender.set_theta(2.4_f32);
+            });
+            Mono::delay(500u32.millis().into()).await;            
+        }
+    }
+    
+    #[task(priority = 3, shared = [sender, candriver])]
+    /// This should enqueue on the Shared `Sender` whenever a 
+    /// sensor reads a new value.
+    async fn send_updates(mut cx: send_updates::Context){
+        // Loop and dequeue, and transmit on the can bus.
+        loop {
+            cx.shared.sender.lock(|sender|{
+                if let Some(data) = sender.dequeue(){
+                    // Value was found in buffer [TRANSMIT OVER CAN]:
+                    cx.shared.candriver.lock(|can|{
+                        can.transmit(&data);
+                    });
+                } 
+            });
+            //Mono::delay(100u32.millis().into()).await;            
+
+        }
+    }
+}
+
+>>>>>>> 86eed1f (rebasing with main)
